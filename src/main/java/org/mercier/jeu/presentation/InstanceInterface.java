9ee2abd838bab0ffdc7b6be56377694cc4c07b86
Bastien MERCIER <method1990@gmail.com>
2014-02-26 23:35:24 +0100

    Début interface graphique

--- src/main/java/org/mercier/jeu/presentation/InstanceInterface.java
+++ src/main/java/org/mercier/jeu/presentation/InstanceInterface.java
@@ -1,6 +1,9 @@
 package org.mercier.jeu.presentation;
 
+
 public class InstanceInterface {
 
-	
+	public static void main(String[] args) {
+		new Interface();
+	}
 }
--- src/main/java/org/mercier/jeu/presentation/Interface.java
+++ src/main/java/org/mercier/jeu/presentation/Interface.java
@@ -1,10 +1,49 @@
 package org.mercier.jeu.presentation;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 public class Interface extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 
+	
+	private JMenuBar barMenu;
+	private JMenu partie;
+	private JMenuItem nouvellePartie;
+	private JMenuItem connexion;
+	
+	public Interface(){
+		super("Button Up Game");
+		setSize(new Dimension(400, 200));
+		setLocationRelativeTo(null);
+		
+		barMenu = new JMenuBar();
+		
+		partie = new JMenu("Jeu");
+		connexion = new JMenuItem("Connexion");
+		connexion.setAccelerator(KeyStroke.getKeyStroke("meta alt C"));
+		partie.add(connexion);
+		
+		nouvellePartie = new JMenuItem("Nouvelle Partie");
+		nouvellePartie.setAccelerator(KeyStroke.getKeyStroke("meta N"));
+		partie.add(nouvellePartie);
+		barMenu.add(partie);
+		
+		
+		setJMenuBar(barMenu);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public JMenuItem getNouvellePartie() {
+		return nouvellePartie;
+	}
 	
 }
